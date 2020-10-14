ee04539603d21ed9a4af469fd31f4991a71a0328
Mahmut Karaca <mahmutkaraca95@gmail.com>
2020-10-15 01:45:28 +0300

    add start_qemu function to exposed API

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import SenderMQ from './sender';
 import ReceiverMQ from './receiver';
 import { QemuMessage } from './qemu_mq_types';
+import { start_qemu } from './apprunner/qemu_runner';
 
 export default {
     SenderMQ,
-    ReceiverMQ
+    ReceiverMQ,
+    start_qemu
 };
